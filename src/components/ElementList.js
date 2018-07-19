aaa52dd5b61eb1d4335c82714efe9248f69ae276
Puran chand <pchand757@gmail.com>
2018-07-20 02:03:32 +0530

    add propTypes and defaultProps

--- src/components/ElementList.js
+++ src/components/ElementList.js
@@ -1,36 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ElementList = ({elements,onMovableActive,onMovableDown}) => {
 
 	return (
-		<div>
-			{
-                elements.map(
-                    (element, index) => {
-                        const { component, props, position} = element
-                        const ComponentToRender = component
-                        const ComponentProps = {
-                            ...props,
-                            position
-                        }
-
-                        return (
-                            <ComponentToRender
-                                key={index}
-                                onMovableActive={onMovableActive(index)}
-                                onMovableDown={onMovableDown}
-                                {...ComponentProps}
-                            />
-                        )
-                    }
+			
+        elements.map(
+            (element, index) => {
+                const { component, props, position} = element
+                const ComponentToRender = component
+                const ComponentProps = {
+                    ...props,
+                    position
+                }
+                return (
+                    <ComponentToRender
+                        key={index}
+                        onMovableActive={onMovableActive(index)}
+                        onMovableDown={onMovableDown}
+                        {...ComponentProps}
+                    />
                 )
             }
-		</div>
-		)
+        )
+	)
 }
 
-// ElementList.propTyps= {
-    
-// }
+ElementList.propTypes = {
+    elements:PropTypes.array.isRequired,
+    onMovableActive:PropTypes.func.isRequired,
+    onMovableDown:PropTypes.func.isRequired
+
+}
+
+ElementList.defaultProps = {
+    elements:[{
+        component:'',
+        props: {},
+        position:{
+            x:0,y:0
+        }
+    }],
+    onMovableActive:() => void(0),
+    onMovableDown:() => void(0)
+}
 
 export default ElementList;
